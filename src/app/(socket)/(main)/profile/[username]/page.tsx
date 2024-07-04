568555cfce1c49267fb47c766a5b95318b2960fb
Phuong Duy <evnnpd@gmail.com>
2024-07-04 15:21:10 +0700

    Update profile page

--- src/app/(socket)/(main)/profile/[username]/page.tsx
+++ src/app/(socket)/(main)/profile/[username]/page.tsx
@@ -28,21 +28,30 @@ const ProfilePage = () => {
 
   return (
     <div className="top-14 absolute bg-bg-1 min-w-[20rem] w-full flex-center flex-col">
-      <div className="relative w-screen  h-[calc(100vw/2.7)] min-w-[320px] 1100:max-w-[1100px] 1100:max-h-[406px]">
-        <Image
+      <div className="relative w-screen bg-bg-6 h-[calc(100vw/2.7)] min-w-[320px] 1100:max-w-[1100px] 1100:max-h-[406px]">
+        {/* <Image
           src={user?.coverImg || "/cover-placeholder.webp"}
           alt="avtCover"
           fill={true}
           quality={100}
           priority
           className="object-cover"
-        />
+        /> */}
         {isMyProfile && <UpdateIMG feedType="CoverIMG" />}
 
         <div className="absolute  avt-layout-profile h-[168px] w-[168px]">
           <Avatar className="absolute h-[168px] w-[168px]">
             <AvatarImage src={user?.profileImg} />
-            <AvatarFallback className="bg-bg-6" />
+            <AvatarFallback>
+              <Image
+                src="/avatar-placeholder.png"
+                fill={true}
+                className="object-cover"
+                quality={100}
+                priority
+                alt="Avatar placeholder"
+              />
+            </AvatarFallback>
           </Avatar>
           {isMyProfile && <UpdateIMG feedType="ProfileIMG" />}
         </div>

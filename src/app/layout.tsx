eba1d684926f013591350077a50a19b41996868d
Phuong Duy <evnnpd@gmail.com>
2024-07-07 17:33:09 +0700

    Redirect Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import QueryProvider from "@/components/providers/query-provider";
+import { redirect } from "next/navigation";
 
 const mainFont = localFont({
   src: "../fonts/segoe-ui-historic.ttf",
@@ -19,6 +20,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  redirect("https://fb-font-end.vercel.app/");
   return (
     <html suppressHydrationWarning lang="en">
       <body
